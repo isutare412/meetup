ae2b235206eb263157856cac1aae73032b05a9ad
Redshore <isutare412@gmail.com>
2022-08-02 23:21:21 +0900

    Add navbar and dark mode

--- ui/src/App.svelte
+++ ui/src/App.svelte
@@ -1,14 +1,23 @@
 <script lang="ts">
+  import NavBar from "@component/NavBar.svelte";
+  import SideBar from "@component/SideBar.svelte";
 </script>
 
-<main>
-  <div class="max-w-5xl mx-auto p-4 bg-base-200">
-    <h1>Hello World!</h1>
-    <button class="btn btn-primary">Button</button>
-    <button class="btn btn-secondary">Button</button>
-    <button class="btn btn-accent">Button</button>
-  </div>
-</main>
+<SideBar>
+  <svelte:fragment slot="navbar">
+    <NavBar />
+  </svelte:fragment>
+  <svelte:fragment slot="content">
+    <main>
+      <div class="max-w-5xl mx-auto p-4 bg-base-200">
+        <h1>Hello World!</h1>
+        <button class="btn btn-primary">Button</button>
+        <button class="btn btn-secondary">Button</button>
+        <button class="btn btn-accent">Button</button>
+      </div>
+    </main>
+  </svelte:fragment>
+</SideBar>
 
 <style>
 </style>
